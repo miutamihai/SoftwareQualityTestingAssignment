57ce274089f62817f6417362ebe3e04b6ab9be75
Mihai <miuta.mihai@gmail.com>
2020-02-10 15:49:45 +0000

    added basic form functionality

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
+from validation import InputForm
+from datetime import datetime
+from werkzeug.utils import redirect
+from config import Config
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+car_insurance_quote = 0
 
-message = 'I work'
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def main_page():
+    form = InputForm()
+    if form.validate_on_submit():
+        return redirect(url_for('result_page', form=form))
     return render_template('index.html')
+
+
+@app.route('/results', methods=['GET', 'POST'])
+def result_page():
+    if request.method == 'POST':
+        return 'POST'
+    else:
+        return 'GET'
 
 
 if __name__ == '__main__':
