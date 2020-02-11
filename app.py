255478b6cf4af748e3d111c0d624d098a81f1955
Mihai <miuta.mihai@gmail.com>
2020-02-11 14:46:06 +0000

    fixed form and got drivers age

--- app.py
+++ app.py
@@ -1,29 +1,25 @@
 from flask import Flask, render_template, url_for, request
+from dateutil.relativedelta import relativedelta
 from validation import InputForm
 from datetime import datetime
 from werkzeug.utils import redirect
 from config import Config
-
 
 app = Flask(__name__)
 app.config.from_object(Config)
 car_insurance_quote = 0
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def main_page():
-    form = InputForm()
-    if form.validate_on_submit():
-        return redirect(url_for('result_page', form=form))
     return render_template('index.html')
 
 
-@app.route('/results', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def result_page():
-    if request.method == 'POST':
-        return 'POST'
-    else:
-        return 'GET'
+    driver_age = relativedelta(datetime.date(datetime.now()), datetime.strptime(request.form['dateOfBirth'],
+                                                                                '%Y-%m-%d')).years
+    return str(driver_age)
 
 
 if __name__ == '__main__':
